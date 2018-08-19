563e5a3c97c6c07bbdd1caf849a974e2e1de7498
hardeep19 <42134363+hardeep19@users.noreply.github.com>
2018-08-19 20:59:16 +0530

    Add files via upload

---
+++ stack.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+
+typedef int DATA;
+
+void push(DATA Stack[], int size, int* TOP, DATA x)
+{
+	if(*TOP==size-1)
+		printf("Error:Overflow: Stack is full");
+	else
+		Stack[++*TOP]=x;
+	return;
+}
+
+DATA pop(DATA Stack[], int* TOP)
+{
+	if(*TOP==-1)
+		printf("Error:Underflow: Stack is empty");
+	else
+		return Stack[(*TOP)--];
+	return 0;
+}
+
+
+int main()
+{
+	DATA Stack[5]={1,2};
+	int size=5;
+	int TOP=1;
+
+	push(Stack,size,&TOP,5);
+	push(Stack,size,&TOP,7);
+	push(Stack,size,&TOP,6);
+	printf(" poped:%d ",pop(Stack,&TOP));
+	printf(" poped:%d ",pop(Stack,&TOP));
+	printf(" poped:%d ",pop(Stack,&TOP));
+	printf(" poped:%d ",pop(Stack,&TOP));
+	printf(" poped:%d ",pop(Stack,&TOP));
+
+	return 0;
+}
